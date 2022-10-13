6c5e4530c6ad72bcb8474c5c986615039a02495e
Your Name <okoshinshil@gmail.com>
2022-10-13 15:35:29 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - prints a phrase
  * Return: 0 if exited properly, else non zero
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
