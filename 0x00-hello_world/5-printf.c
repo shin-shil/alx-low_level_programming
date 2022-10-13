d9c8cd58d854eb8696f4d3ddb2bcb7cba12398ec
Your Name <okoshinshil@gmail.com>
2022-10-13 15:40:03 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a phrase with proper grammer
+ * Return:0 if no errors encountered
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
