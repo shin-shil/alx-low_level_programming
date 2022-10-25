1542cf41376f879b71ce32f4bd1a3ec1827924c6
Your Name <okoshinshil@gmail.com>
2022-10-24 19:26:09 -0700

    char *_strcpy(char *dest, char *src);

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int length = 0;
 
-	while (*(src + length) != ‘\0’)
+	while (*(src + length) != '\0')
 	{
 		*(dest + length) = *(src + length);
 		length++;
 	}
 
-	*(dest + length) = ‘\0’;
+	*(dest + length) = '\0';
 	return (dest);
 }
 
