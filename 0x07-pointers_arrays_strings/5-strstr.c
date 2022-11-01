2a3ad00a2c80137f442150cbbe79f58003d7e998
Your Name <okoshinshil@gmail.com>
2022-10-31 22:34:37 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * strstr - Locates a substring.
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
+ *
+ * Return: a pointer if substring located
+ * and null if it isnt
+ */
+
+char *strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+				{
+					return (haystack);
+				}
+
+				index++;
+			}
+			while{
+				haystack[index] == needle[index];
+			}
+		}
+		haystack++
+	}
+	return ('\0')
+}
